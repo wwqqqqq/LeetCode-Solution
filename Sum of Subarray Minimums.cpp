2795b0f69de9772058464438927914e49b2409c5
wwqqqqq <weiq618@gmail.com>
2019-07-11 18:57:11 +0800

    2019/7/11

--- Sum of Subarray Minimums.cpp
+++ Sum of Subarray Minimums.cpp
@@ -1,5 +1,45 @@
 class Solution {
 public:
+    /* TACT 1 O(n) */
+    int sumSubarrayMins(vector<int>& A) {
+        vector<int> result(A.size(), 0);
+        vector<int> ofst(A.size(), 0);
+        // ofst[i] = j - i, where j is the minimum index greater than i while A[j] < A[i]
+        ofst[A.size() - 1] = 0;
+        for(int i = A.size() - 2; i >= 0; i--) {
+            if(A[i] > A[i+1]) {
+                ofst[i] = 1;
+                continue;
+            }
+            int r = i + 1;
+            while(r < A.size() && A[r] >= A[i]) {
+                if(ofst[r] == 0) {
+                    ofst[i] = 0;
+                    break;
+                }
+                r = r + ofst[r];
+            }
+            if(r < A.size() && A[r] < A[i])
+                ofst[i] = r - i;
+        }
+        // result[i] is the sum of mins of all subarrays that begin with A[i]
+        result[A.size() - 1] = A[A.size() - 1];
+        for(int i = A.size() - 2; i >= 0; i--) {
+            if(ofst[i] == 0) {
+                result[i] = (A.size() - i) * A[i];
+                continue;
+            }
+            result[i] = A[i] * ofst[i] + result[i + ofst[i]];
+            result[i] = result[i] % 1000000007;
+        }
+        int sum = 0;
+        for(int i = 0; i < result.size(); i++) {
+            sum += result[i];
+            sum = sum % 1000000007;
+        }
+        return sum;
+    }
+    /* TACT 2 O(nlogn) ~ O(n^2)
     int MIN(int a, int b) {
         if(a < b)
             return a;
@@ -46,8 +86,9 @@ public:
     }
     int sumSubarrayMins(vector<int>& A) {
         return sumSubarrayMins(A, 0, A.size());
-    }
-    /*int min(int a, int b) {
+    }*/
+    /* TACT 3 O(n^2)
+    int min(int a, int b) {
         if(a < b)
             return a;
         return b;
