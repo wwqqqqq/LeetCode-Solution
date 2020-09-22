b8f985e7940ec0862770a641cb438d25833f874a
wwqqqqq <weiq618@gmail.com>
2020-09-21 22:02:35 -0700

    2020/09/21

--- Combination Sum.cpp
+++ Combination Sum.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-    void findCS(vector<int> candidates, int target, vector<vector<int>>& result)
-    {
-        if(candidates.size()==0) return;
-        vector<vector<int>> temp;
-        int x=candidates[candidates.size()-1];
-        candidates.pop_back();
-        int i=0;
-        while(x*i<=target)
-        {
-            if(x*i==target)
-            {
-                vector<int> temp;
-                for(int j=0;j<i;j++) temp.push_back(x);
-                result.push_back(temp);
-            }
-            else
-            {
-                vector<vector<int>> temp;
-                findCS(candidates,target-x*i,temp);
-                for(auto iter=temp.begin();iter!=temp.end();iter++)
-                {
-                    for(int j=0;j<i;j++) (*iter).push_back(x);
-                }
-                result.insert(result.end(),temp.begin(),temp.end());
-            }
-            i++;
+    vector<vector<int>> result;
+    void combinationSum(vector<int>& candidates, int begin, int target, vector<int> prefix) {
+        if(target == 0) {
+            result.push_back(prefix);
+            return;
+        }
+        if(begin >= candidates.size()) {
+            return;
+        }
+        int num = 0;
+        while(num <= target) {
+            combinationSum(candidates, begin + 1, target - num, prefix);
+            num += candidates[begin];
+            prefix.push_back(candidates[begin]);
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>> result;
-        findCS(candidates,target,result);
+        vector<int> prefix;
+        combinationSum(candidates, 0, target, prefix);
         return result;
     }
 };
